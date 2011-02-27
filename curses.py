07a8618a9d140b5c77415eef83c2173ccaa2f800
PiPeep <odetopi.e@gmail.com>
2011-02-27 15:51:05 -0500

    Added more cuss words

--- curses.py
+++ curses.py
@@ -1,7 +1,8 @@
 import re
 
-curses = ["fuck", "fucking", "fucked", "damn", "damnation", "dammit",
-          "goddammit", "shit", "screwed", "shoes"]
+curses = ["fuck", "fucking", "fucked", "damn", "damnation", "dammit", "crap",
+          "goddammit", "shit", "screwed", "shoes", "ass", "asshat",
+          "donkey kong"]
 regex_curses = []
 
 for c in curses:
