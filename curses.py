4687d123162ad23f77baf64fad418ce40d1c6e47
PiPeep <odetopi.e@gmail.com>
2011-03-03 20:29:13 -0500

    I forgot a comma in the curses list

--- curses.py
+++ curses.py
@@ -3,8 +3,8 @@ import re
 curses = [re.compile("(.*|^)fuck(.*|$)"), "damn", "damnation", "dammit", "crap",
           "goddammit", "shit", "screwed", "shoes", "ass", "asshat", "jackass",
           "donkey kong", "tits", "sonuvabitch", "dickhead", "gay", "horny",
-          "fag", "faggot", "penis", "vagina" "cunt", "bitch", "asshole", "slut",
-          "whore", "pussy"]
+          "fag", "faggot", "penis", "vagina", "cunt", "bitch", "asshole",
+          "slut", "whore", "pussy"]
 regex_curses = []
 
 for c in curses:
